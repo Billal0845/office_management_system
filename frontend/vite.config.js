afa537ee397598094db68ae8a1db95a86868502a
Billal0845 <billalhosen.developer@gmail.com>
2026-03-20 14:53:03 +0600

    First in CICD fixed 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    // This sends the React build to the Laravel public folder
+    outDir: "../backend/public",
+    emptyOutDir: false, // Keep Laravel's index.php and .htaccess
+  },
 });
